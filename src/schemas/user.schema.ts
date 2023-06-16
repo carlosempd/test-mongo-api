e5b6794e3d44a6a7c4fdd40f96845a5b9481b21e
Carlos <carlosempd@gmail.com>
2023-06-16 09:47:06 -0400

    create user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop()
+  firstname: string;
+
+  @Prop()
+  lastname: string;
+
+  @Prop()
+  username: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  profilePhoto: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
