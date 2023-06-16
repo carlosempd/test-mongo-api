79d8d8007556b8fdcf8cf0d4b4cbe6cc60e48892
Carlos <carlosempd@gmail.com>
2023-06-16 18:02:05 -0400

    configure app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { MediaModule } from './media/media.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://carlos:12345@localhost:27017'),
     UsersModule,
     MediaModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
