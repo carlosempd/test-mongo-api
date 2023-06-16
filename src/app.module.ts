b8d665ee6103e187c7eb22714336d939a2f4649d
Carlos <carlosempd@gmail.com>
2023-06-16 15:03:26 -0400

    configure media module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { MediaModule } from './media/media.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://carlos:12345@localhost:27017'),
     UsersModule,
+    MediaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
