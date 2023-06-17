2c2cc4dd431f2cb1378b38a05d5b48672666d4b2
Carlos <carlosempd@gmail.com>
2023-06-16 23:12:23 -0400

    configure mongoose connection with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,18 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { MediaModule } from './media/media.module';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { bcryptConfig, jwtConfig } from './core/config/config';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://carlos:12345@localhost:27017'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => ({
+        uri: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@localhost:27017`
+      })
+    }),
     ConfigModule.forRoot({ 
       isGlobal: true,
       envFilePath: '.env',
