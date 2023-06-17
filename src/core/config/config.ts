8bcd1478a2289bd802271d0444bbda4aacbf2277
Carlos <carlosempd@gmail.com>
2023-06-16 22:25:24 -0400

    create config for bcrypt

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -3,3 +3,7 @@ import { registerAs } from "@nestjs/config";
 export const jwtConfig = registerAs('jwt', () => ({
     secret: process.env.JWT_SECRET
 }))
+
+export const bcryptConfig = registerAs('salt', () => ({
+    rounds: process.env.SALT_ROUNDS
+}))
